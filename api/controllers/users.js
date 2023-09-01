742fd6ad6ffb77d038cd79e68d3dc04d46dc5307
EmersonTI733 <emers733@gmail.com>
2023-08-31 23:39:16 -0400

    atualizado1

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -23,17 +23,18 @@ router.post('/login', async (req, res)=>{
         });
         if(user){
             res.status(200).json(data={
+                matricula:user.matricula,
                 message:"Usuario encontrado!",
                 code:200
             })
         }else{
-            res.status(400).json({
+            res.status(400).json(data={
                 message:"Conta nao encontrada, verifique dados de login",
                 code:400
             })
         }
     }else{
-            res.status(401).json({
+            res.status(401).json(data={
                 message:"preencha todos os campos",
                 code:401
             })
@@ -283,57 +284,65 @@ router.get('/listar_residencias',async (req, res)=>{
 
 //rota de edicao
 router.put('/editar_residencia', async (req, res)=>{
-    // dados recebidos
-    // dados recebidos: nome_titular, cpf, cep, bairro, rua, numero, email, telefone
-    var dados = req.body;
-    if(dados){
-        const update = await db.dados_residencias.findOne({ id: dados.id });//trazer o item a ser editado
-        //novo valor
-        if(dados.nome_titular){
-            update.nome_titular = dados.nome_titular;
-        }
-        if(dados.cpf){
-            update.cpf = dados.cpf;
-        }
-        if(dados.cep){
-            update.cep = dados.cep;
-        }
-        if(dados.bairro){
-            update.bairro = dados.bairro;
-        }
-        if(dados.rua){
-            update.rua = dados.rua;
-        }
-        if(dados.numero){
-            update.numero = dados.numero;
-        }
-        if(dados.email){
-            update.email = dados.email;
-        }
-        if(dados.telefone){
-            update.telefone = dados.telefone;
-        }
-
-        await update.save({ fields: [
-            'nome_titular', 'cpf', 'cep', 'bairro', 'rua', 'numero', 'email', 'telefone'
-        ] });
-
-        await update.reload();//salvar novo dado
-
-        res.json(
-            {
-                message:"dados foram salvos"
+        // Dados recebidos: nome_titular, cpf, cep, bairro, rua, numero, email, telefone
+        var dados = req.body;
+        if (
+          dados.nome_titular ||
+          dados.cpf ||
+          dados.cep ||
+          dados.bairro ||
+          dados.rua ||
+          dados.numero ||
+          dados.email ||
+          dados.telefone
+        ) {
+          const update = await db.dados_residencias.findOne({
+            where: { id: dados.id },
+          });
+      
+          if (update) {
+            // Atualize os valores apenas se eles estiverem presentes nos dados recebidos
+            if (dados.nome_titular) {
+              update.nome_titular = dados.nome_titular;
             }
-        )
-    }else{
-        res.json(
-            {
-                message:"nada pra salvar"
+            if (dados.cpf) {
+              update.cpf = dados.cpf;
             }
-        )
-    }
-    
-
+            if (dados.cep) {
+              update.cep = dados.cep;
+            }
+            if (dados.bairro) {
+              update.bairro = dados.bairro;
+            }
+            if (dados.rua) {
+              update.rua = dados.rua;
+            }
+            if (dados.numero) {
+              update.numero = dados.numero;
+            }
+            if (dados.email) {
+              update.email = dados.email;
+            }
+            if (dados.telefone) {
+              update.telefone = dados.telefone;
+            }
+      
+            // Salve as alterações no banco de dados
+            await update.save();
+      
+            res.json({
+              message: "Dados foram salvos",
+            });
+          } else {
+            res.json({
+              message: "ID não encontrado",
+            });
+          }
+        } else {
+          res.json({
+            message: "Nada para salvar",
+          });
+        }
        
 })
 router.put('/editar_condominio', async (req, res)=>{
@@ -341,7 +350,13 @@ router.put('/editar_condominio', async (req, res)=>{
     // dados recebidos: nome_condominio, cnpj, cep, bairro, rua, numero, email, telefone, sindico
     var dados = req.body;
     if(dados){
-        const update = await db.dados_condominios.findOne({ id: dados.id });//trazer o item a ser editado
+        const update = await db.dados_condominios.findOne(
+            {
+                where:{
+                    id: dados.id
+                }
+            }
+            );//trazer o item a ser editado
         //novo valor
         if(dados.nome_condominio){
             update.nome_condominio = dados.nome_condominio;
@@ -371,20 +386,16 @@ router.put('/editar_condominio', async (req, res)=>{
             update.sindico = dados.sindico;
         }
 
-        await update.save({ fields: [
-            'nome_condominio', 'cnpj', 'cep', 'bairro', 'rua', 'numero', 'email', 'telefone', 'sindico'
-        ] });
-
-        await update.reload();//salvar novo dado
+        await update.save();
 
         res.json(
-            {
+            data={
                 message:"dados foram salvos"
             }
         )
     }else{
         res.json(
-            {
+            data={
                 message:"nada pra salvar"
             }
         )
@@ -398,7 +409,11 @@ router.put('/editar_empresa', async (req, res)=>{
     // dados recebidos: nome_fantasia, cnpj, cep, bairro, rua, numero, razao_social, ceo, email, telefone
     var dados = req.body;
     if(dados){
-        const update = await db.dados_empresas.findOne({ id: dados.id });//trazer o item a ser editado
+        const update = await db.dados_empresas.findOne({
+            where:{
+                id: dados.id
+            }
+        });//trazer o item a ser editado
         //novo valor
         if(dados.nome_fantasia){
             update.nome_fantasia = dados.nome_fantasia;
@@ -431,20 +446,16 @@ router.put('/editar_empresa', async (req, res)=>{
             update.telefone = dados.telefone;
         }
 
-        await update.save({ fields: [
-            'nome_fantasia', 'cnpj', 'cep', 'bairro', 'rua', 'numero', 'razao_social', 'ceo', 'email', 'telefone'
-        ] });
-
-        await update.reload();//salvar novo dado
+        await update.save();
 
         res.json(
-            {
+            data={
                 message:"dados foram salvos"
             }
         )
     }else{
         res.json(
-            {
+            data={
                 message:"nada pra salvar"
             }
         )
